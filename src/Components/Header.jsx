4e3b506199b5de15539b77f9f326ec5b067fda07
Adityakanojia24 <adityakanojia63830@gmail.com>
2022-10-31 11:28:44 +0530

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// import Navbar from '../Components/Navbar';
+
+function Header() {
+  return (
+    <div id='main'>
+<div className='name'>
+    <h1>SPORTS SUB COUNCIL</h1>
+    <h2><span>Madan Mohan Malaviya University of Technology , Gorakhpur (U.P.)</span></h2>
+    </div>
+    </div>
+  )
+}
+
+export default Header;
