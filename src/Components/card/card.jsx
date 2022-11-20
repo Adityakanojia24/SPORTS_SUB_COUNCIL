ffab0406aecb913a091283d034674ff8abcf9c5f
Abhay Verma <abhayverma356@gmail.com>
2022-11-20 15:39:55 +0530

    bg image

--- src/Components/card/card.jsx
+++ src/Components/card/card.jsx
@@ -1,60 +1,99 @@
 import React from "react";
 import "./stylecard.css";
 import Cricket from "./cricket.png";
-import Bg from "./image/cardbg.jpg";
+import football from "./image/football.jpg";
+import basketball from "./image/basketball.jpg";
+import tabletennis from "./image/badmintontable.jpg";
+import hockey from "./image/hockey.jpg";
+import vollyball from "./image/vollyball.jpg";
 
 const Card = () => {
   return (
     <>
       <div>
-        <body style={{ backgroundImage: `url(${Bg})` }}>
-          <section>
-            <div class="container">
-              <div class="card">
-                <div class="content">
-                  <div class="imgBx">
-                    <img src={Cricket} alt="basketball" />
-                  </div>
+        {/* <body style={{ backgroundImage: `url(${Bg})` }}> */}
+
+        <section className="bg-image">
+          <center className>
+            <h1>OUR EVENT </h1>
+          </center>
+          <div className="container">
+            <div className="card">
+              <div className="content">
+                <div className="imgBx">
+                  <img src={Cricket} alt="basketball" />
                 </div>
-                <ul class="sci">
-                  <li>
-                    <h1 style={{ color: "white" }}>Cricket</h1>
-                  </li>
-                </ul>
               </div>
-              <div class="card">
-                <div class="content">
-                  <div class="imgBx">
-                    <img
-                      src="https://image.flaticon.com/icons/png/256/4213/4213736.png"
-                      alt="football"
-                    />
-                  </div>
-                </div>
-                <ul class="sci">
-                  <li>
-                    <h1 style={{ color: "white" }}>Football</h1>
-                  </li>
-                </ul>
-              </div>
-              <div class="card">
-                <div class="content">
-                  <div class="imgBx">
-                    <img
-                      src="https://image.flaticon.com/icons/png/256/4213/4213641.png"
-                      alt="cricket"
-                    />
-                  </div>
-                </div>
-                <ul class="sci">
-                  <li>
-                    <h1 style={{ color: "white" }}>Basketball</h1>
-                  </li>
-                </ul>
-              </div>
+              <ul className="sci">
+                <li>
+                  <h1 style={{ color: "white" }}>Cricket</h1>
+                </li>
+              </ul>
             </div>
-          </section>
-        </body>
+            <div className="card">
+              <div className="content">
+                <div className="imgBx">
+                  <img src={football} alt="football" />
+                </div>
+              </div>
+              <ul className="sci">
+                <li>
+                  <h1 style={{ color: "white" }}>Football</h1>
+                </li>
+              </ul>
+            </div>
+            <div className="card">
+              <div className="content">
+                <div className="imgBx">
+                  <img src={basketball} alt="basketball" />
+                </div>
+              </div>
+              <ul className="sci">
+                <li>
+                  <h1 style={{ color: "white" }}>Basketball</h1>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="container">
+            <div className="card">
+              <div className="content">
+                <div className="imgBx">
+                  <img src={tabletennis} alt="basketball" />
+                </div>
+              </div>
+              <ul className="sci">
+                <li>
+                  <h1 style={{ color: "white" }}>Tabletennis</h1>
+                </li>
+              </ul>
+            </div>
+            <div className="card">
+              <div className="content">
+                <div className="imgBx">
+                  <img src={hockey} alt="football" />
+                </div>
+              </div>
+              <ul className="sci">
+                <li>
+                  <h1 style={{ color: "white" }}>Hockey</h1>
+                </li>
+              </ul>
+            </div>
+            <div className="card">
+              <div className="content">
+                <div className="imgBx">
+                  <img src={vollyball} alt="basketball" />
+                </div>
+              </div>
+              <ul className="sci">
+                <li>
+                  <h1 style={{ color: "white" }}>vollyball</h1>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </section>
       </div>
     </>
   );
